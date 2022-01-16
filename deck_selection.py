e156f0b5c3a229f0d65fcc8ce9c37886189399ed
amaiebi <89315879+amaiebi@users.noreply.github.com>
2022-01-16 18:23:37 +0900

    Update deck_selection.py

--- deck_selection.py
+++ deck_selection.py
@@ -33,6 +33,10 @@ async def on_message(message):
     	response = random.sample(megami_tatsujin, 2)
     	await message.channel.send(response)        
         
+    if message.content.startswith('!furuyoni6'):
+    	megami_dairoku = {"ユリナ", "サイネ", "ヒミカ", "トコヨ", "オボロ", "ユキヒ", "シンラ", "ハガネ", "チカゲ", "クルル", "サリヤ", "ライラ", "ウツロ", "ユリナA1", "サイネA1", "ヒミカA1", "トコヨA1", "ホノカ", "オボロA1", "チカゲA1", "ウツロA1", "コルヌ", "ヤツハ", "シンラA1", "クルルA1", "サイネA2", "ハツミ", "ミズキ", "ユキヒA1", "サリヤA1", "ライラA1", "メグミ", "カナヱ", "トコヨA2", "ハガネA1", "ホノカA1", "カムヰ", "レンリ", "クルルA2", "ヤツハA1", "ハツミA1"}
+    	response = random.sample(megami_dairoku, 2)
+    	await message.channel.send(response)            
         
 token = getenv('DISCORD_BOT_TOKEN')
 client.run(token)
